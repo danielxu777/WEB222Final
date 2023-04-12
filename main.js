6ebd2e5cf2cb0286c0c31419300c92441b5a073b
danielxu777 <danielxu0728@gmail.com>
2023-04-11 22:17:52 -0400

    Modified

--- main.js
+++ main.js
@@ -1,3 +1,63 @@
+//index.html
+//This part is for a function that if the user would like to see big pictures
+//from the home page, click the picture, the 
+function enlargeImage(image) {
+  // Create a new div element to hold the enlarged image
+  const overlay = document.createElement("div");
+  overlay.classList.add("image-overlay");
+
+  // Create a new image element to display the enlarged image
+  const enlargedImage = document.createElement("img");
+  enlargedImage.src = image.src;
+  enlargedImage.alt = image.alt;
+  overlay.appendChild(enlargedImage);
+
+  // Add the overlay to the body of the page
+  document.body.appendChild(overlay);
+
+  // Disable scrolling while the overlay is visible
+  document.body.style.overflow = "hidden";
+
+  // Close the overlay when the user clicks outside of it
+  overlay.onclick = function() {
+    document.body.removeChild(overlay);
+    document.body.style.overflow = "auto";
+  }
+}
+
+const form = document.getElementById('newsletter-form');
+const emailInput = document.getElementById('email');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent the form from submitting normally
+
+  // Validate the email address
+  if (!emailInput.checkValidity()) {
+    alert('Please enter a valid email address.');
+    return;
+  }
+
+  // Submit the form data
+  const formData = new FormData(form);
+  fetch('https://httpbin.org/post', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => {
+    if (response.ok) {
+      alert('Thanks for joining our newsletter!');
+    } else {
+      alert('There was an error submitting the form. Please try again later.');
+    }
+  })
+  .catch(error => {
+    console.error(error);
+    alert('There was an error submitting the form. Please try again later.');
+  });
+});
+
+//reviews.html
+//This part is related to the reviews.html shows the customers' review
 // Get the review data from data.js
 const reviewData = window.reviewData;
 
@@ -61,7 +121,7 @@ function getRatingStars(rating) {
     return stars;
 }
 
-// Format a date string in the format YYYY-MM-DD to a more readable format
+//Format a date string in the format YYYY-MM-DD to a more readable format
 function formatDate(dateString) {
     const date = new Date(dateString);
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
@@ -77,33 +137,8 @@ function getCurrentDate() {
     return `${year}-${month}-${day}`;
 }
 
-const form = document.getElementById('newsletter-form');
-const emailInput = document.getElementById('email');
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent the form from submitting normally
 
-  // Validate the email address
-  if (!emailInput.checkValidity()) {
-    alert('Please enter a valid email address.');
-    return;
-  }
 
-  // Submit the form data
-  const formData = new FormData(form);
-  fetch('https://httpbin.org/post', {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => {
-    if (response.ok) {
-      alert('Thanks for joining our newsletter!');
-    } else {
-      alert('There was an error submitting the form. Please try again later.');
-    }
-  })
-  .catch(error => {
-    console.error(error);
-    alert('There was an error submitting the form. Please try again later.');
-  });
-});
+
+
